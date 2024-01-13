066c10dda78836d974a8ac8001a99b94e22a6b7c
RealRainbuu <aaronemoryd@gmail.com>
2024-01-12 23:10:55 -0500

    rainbuu

---
+++ dictionary.js
@@ -0,0 +1,26 @@
+Dictionary = [
+    {
+        "word": "janja",
+        "uses": [
+            {
+                "type": "COMPOUND",
+                "compound": "jan (person) + nja (meow)",
+                "definition": "Catperson and catpeople",
+                "example": "janja li suwi.",
+                "exampleTranslation": "Catpeople are cute."
+            }
+        ]
+    },
+    {
+        "word": "musika",
+        "uses": [
+            {
+                "type": "COMPOUND",
+                "compound": "musi (entertainment) + kalama (sound)",
+                "definition": "Music",
+                "example": "mi kute musika.",
+                "exampleTranslation": "I listen to music."
+            }
+        ]
+    }
+]
