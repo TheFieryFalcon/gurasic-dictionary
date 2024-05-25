bf3e47fcdbc105322d1c20c8817265d218b44e3e
Quantum <72846259+TheFieryFalcon@users.noreply.github.com>
2024-05-25 23:40:34 +0800

    quan vs comma round 3

--- dictionary.js
+++ dictionary.js
@@ -18,19 +18,19 @@ Dictionary = [
                 "definition": "An exclamation of mirth and humor",
                 "example": '"Ligma balls" "lmao"',
                 "exampleTranslation": '"Ligma balls" "That is really funny"'
-            }
+            },
             {
                 "type": "Expression",
                 "definition": "An expression of incredulity at a situation",
                 "example": '"My house just burned down lmao"',
                 "exampleTranslation": '"My house just burned down, can you believe it?"'
-            }
+            },
             {
                 "type": "Exclaimation",
                 "definition": "An exclamation of pity and/or mocking at someone's situation, usually in all caps",
                 "example": '"My house just burned down lmao" "LMAO"',
                 "exampleTranslation": '"My house just burned down lmao" "I feel sorry for you"'
-            }
+            },
             {
                 "type": "Other",
                 "definition": "An otherwise meaningless word that serves to add a whimsical, alleviating, or optimistic tone to the statement",
